229d4e1da716e19395ee72d65408260ab3d9c6da
nyanklc <noyanerdinkilic@gmail.com>
2023-10-06 12:32:14 +0300

    function signature

--- init.lua
+++ init.lua
@@ -72,9 +72,29 @@ require("lazy").setup({
   {'zefei/cake16'},
   {'ajgrf/parchment'},
   {'rebelot/kanagawa.nvim'},
+  {'wellle/context.vim'},
+
+-- LSP
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {'L3MON4D3/LuaSnip'},
 })
 
 -- plugin setup
+-- LSP
+local lsp_zero = require('lsp-zero')
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+require('lspconfig').lua_ls.setup({})
+require('lspconfig').clangd.setup{}
+-- others
 require('lualine').setup({
 })
 local builtin = require('telescope.builtin')
