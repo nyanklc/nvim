bb20cba20b7307bd685da825bcdbd275230c6a24
Noyan <noyanerdinkilic@gmail.com>
2025-10-11 01:35:14 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -142,6 +142,21 @@ require("lazy").setup({
           desc = "Quickfix List (Trouble)",
         },
       },
+      modes = {
+        preview_float = {
+          mode = "diagnostics",
+          preview = {
+            type = "float",
+            relative = "editor",
+            border = "rounded",
+            title = "Preview",
+            title_pos = "center",
+            position = { 0, -2 },
+            size = { width = 0.3, height = 0.3 },
+            zindex = 200,
+          },
+        },
+      },
     },
 })
 
